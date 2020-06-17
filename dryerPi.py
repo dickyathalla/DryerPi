c2e3a784279305f3bdbdf1663166099b359b1c7b
PWRxPSYCHO <pwrxsycho@gmail.com>
2020-06-17 16:47:41 -0400

    initial commit

---
+++ dryerPi.py
@@ -0,0 +1,67 @@
+from gpiozero import DigitalInputDevice
+from time import sleep
+import sys
+import time
+import smtplib
+import ssl
+import datetime
+import logging
+import configparser
+
+
+vibrationSensor = DigitalInputDevice(17)
+logging.basicConfig(filename="logfile.log", level=logging.INFO)
+configparser = configparser.ConfigParser()
+configparser.read('config.txt')
+
+credentials = 'Credentials'
+
+
+def vibration():
+    if vibrationSensor.is_active:
+        logging.info(
+            "Dryer motion detected sleeping for 10 seconds: %s", datetime.datetime.now().strftime("%H:%M:%S- %b %d %Y"))
+        sleep(10)
+        if vibrationSensor.is_active:
+            logging.info(
+                "Dryer motion detected after sleeping for 10 seconds, exiting function: %s", datetime.datetime.now().strftime("%H:%M:%S- %b %d %Y"))
+            return
+        else:
+            logging.info("Sleeping for 5 sec... ")
+            sleep(5)
+            if vibrationSensor.is_active:
+                logging.info(
+                    "Detected motion after 5 sec, exiting function: %s", datetime.datetime.now().strftime("%H:%M:%S- %b %d %Y"))
+                return
+            else:
+                logging.info("Dryer has stopped, sending email: %s",
+                             datetime.datetime.now().strftime("%H:%M:%S- %b %d %Y"))
+                email()
+
+
+def email():
+    sender_email = sendEmail
+    rec_email = recEmail
+    password = passwd
+    msg = '''Dryer finished'''
+
+    server = smtplib.SMTP(smtp, port)
+    server.starttls()
+    server.login(sender_email, password)
+    logging.info("Login success %s",
+                 datetime.datetime.now().strftime("%H:%M:%S- %b %d %Y"))
+    server.sendmail(sender_email, rec_email, msg)
+    logging.info("Email has been sent to %s : %s" %
+                 (rec_email, datetime.datetime.now().strftime("%H:%M:%S- %b %d %Y")))
+
+
+sendEmail = configparser[credentials]['sender_email']
+passwd = configparser[credentials]['password']
+recEmail = configparser[credentials]['rec_email']
+port = configparser[credentials]['port']
+smtp = configparser[credentials]['smtp']
+
+while True:
+    if vibrationSensor.is_active:
+        sleep(1)
+        vibration()
